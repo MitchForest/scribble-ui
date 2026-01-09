aa067eedf965e21249d9b8da9d6994623435e50a
Mitch Forest <white.mitchell.f@gmail.com>
2026-01-09 16:16:14 -0600

    Add index.ts barrel export, update license

---
+++ registry/index.ts
@@ -0,0 +1,67 @@
+/**
+ * Scribble UI - Hand-drawn component library
+ *
+ * Barrel export for all components
+ */
+
+// Core utilities
+export * from "./lib"
+
+// Components
+export * from "./accordion"
+export * from "./alert-dialog"
+export * from "./avatar"
+export * from "./avatar-picker"
+export * from "./badge"
+export * from "./button"
+export * from "./card"
+export * from "./chart"
+export * from "./checkbox"
+export * from "./dialog"
+export * from "./input"
+export * from "./input-otp"
+export * from "./label"
+export * from "./link"
+export * from "./page-footer"
+export * from "./page-header"
+export * from "./progress"
+export * from "./rating"
+export * from "./select"
+export * from "./selection-card"
+export * from "./sidebar"
+export * from "./skeleton"
+export * from "./table"
+export * from "./tabs"
+export * from "./textarea"
+export * from "./toast"
+export * from "./toggle"
+export * from "./tooltip"
+
+// Annotations
+export * from "./annotation/underline"
+export * from "./annotation/highlight"
+export * from "./annotation/bracket"
+export * from "./annotation/circle"
+export * from "./annotation/box"
+export * from "./annotation/crossed-off"
+
+// Decorative
+export * from "./decorative/arrow"
+export * from "./decorative/badge"
+export * from "./decorative/divider"
+export * from "./decorative/doodle"
+export * from "./decorative/heart"
+export * from "./decorative/logo"
+export * from "./decorative/star"
+export * from "./decorative/sticky-note"
+export * from "./decorative/tape"
+
+// Icons
+export * from "./icons/icon"
+export * from "./icons/close"
+export * from "./icons/hamburger"
+export * from "./icons/plus-minus"
+
+// Backgrounds
+export * from "./backgrounds/notebook"
+export * from "./backgrounds/torn-edge"
